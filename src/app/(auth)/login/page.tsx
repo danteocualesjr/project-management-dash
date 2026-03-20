07d30983ecaf8fc2b474e63a217803eb45568464
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2026-03-20 08:26:34 +0800

    changing ui

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,6 +8,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Loader2 } from 'lucide-react';
+
 import { ROUTES } from '@/lib/constants';
 
 export default function LoginPage() {
